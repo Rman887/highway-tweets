d9431d5ae7f3195bb7736b8f40581d62a1fd5acc
Arman S <Rman887@users.noreply.github.com>
2019-06-07 14:06:36 -0400

    Test beautifulsoup

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    page = requests.get('https://github.com/Rman887/highway-tweets')
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    links = soup.find_all('a')
+
+    link_list = [x.prettify() for x in links]
+
+    return render_template('index.html', text=link_list)
 
 
 if __name__ == '__main__':
